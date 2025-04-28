291f651ce4cadbc24162539a1f1710fd90c7e9ec
judeelfino <165738352+judeelfino@users.noreply.github.com>
2025-04-28 14:18:56 +0000

    feat: generate jwt token

---
+++ app/utils.py
@@ -0,0 +1,24 @@
+from fastapi import Depends, Security
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+
+from database import get_db
+
+SECRET_KEY = "SUPER_SECRET_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
